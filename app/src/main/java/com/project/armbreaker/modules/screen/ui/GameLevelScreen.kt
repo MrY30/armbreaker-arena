2f4de2c25bf47107bf0586a8c4436a5d4da73a5b
Eumelle Jeo Mamacos <jeomamacosschool@gmail.com>
2025-02-25 22:47:04 +0800

    Added Game Levels Screen

---
+++ app/src/main/java/com/project/armbreaker/modules/screen/ui/GameLevelScreen.kt
@@ -0,0 +1,14 @@
+package com.project.armbreaker.modules.screen.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun GameLevelScreen() {
+
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable fun GameLevelScreenPreview() {
+    GameLevelScreen()
+}
